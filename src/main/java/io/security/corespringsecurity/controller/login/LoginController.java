6c7b2edcb6453b8f301fc42529a89dd60aad97b9
jehyunJung <toojey7@gmail.com>
2023-03-09 23:20:37 +0900

    modify: moved signIn function to LoginController

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,26 +1,35 @@
 package io.security.corespringsecurity.controller.login;
 
 
+import io.security.corespringsecurity.domain.dto.AccountDto;
 import io.security.corespringsecurity.domain.entity.Account;
 import io.security.corespringsecurity.security.token.AjaxAuthenticationToken;
+import io.security.corespringsecurity.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
 
-	@RequestMapping(value="/login")
+	@RequestMapping(value={"/login","api/login"})
 	public String login(@RequestParam(value = "error", required = false) String error,
 						@RequestParam(value = "exception", required = false) String exception, Model model){
 		model.addAttribute("error",error);
@@ -55,5 +64,22 @@ public class LoginController {
 		model.addAttribute("exception", exception);
 
 		return "user/login/denied";
+	}
+
+	@GetMapping(value="/signIn")
+	public String createUser() throws Exception {
+		return "user/login/register";
+	}
+
+	@PostMapping(value="/signIn")
+	public String createUser(AccountDto accountDto) throws Exception {
+
+		ModelMapper modelMapper = new ModelMapper();
+		Account account = modelMapper.map(accountDto, Account.class);
+		account.setPassword(passwordEncoder.encode(accountDto.getPassword()));
+
+		userService.createUser(account);
+
+		return "redirect:/";
 	}
 }
--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -1,18 +1,15 @@
 package io.security.corespringsecurity.controller.user;
 
 
-import io.security.corespringsecurity.domain.dto.AccountDto;
 import io.security.corespringsecurity.domain.entity.Account;
 import io.security.corespringsecurity.repository.RoleRepository;
 import io.security.corespringsecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
@@ -24,23 +21,7 @@ public class UserController {
 
 	private final RoleRepository roleRepository;
 
-	@GetMapping(value="/users")
-	public String createUser() throws Exception {
 
-		return "user/login/register";
-	}
-
-	@PostMapping(value="/users")
-	public String createUser(AccountDto accountDto) throws Exception {
-
-		ModelMapper modelMapper = new ModelMapper();
-		Account account = modelMapper.map(accountDto, Account.class);
-		account.setPassword(passwordEncoder.encode(accountDto.getPassword()));
-
-		userService.createUser(account);
-
-		return "redirect:/";
-	}
 
 	@GetMapping(value="/mypage")
 	public String myPage(@AuthenticationPrincipal Account account, Authentication authentication, Principal principal) throws Exception {
