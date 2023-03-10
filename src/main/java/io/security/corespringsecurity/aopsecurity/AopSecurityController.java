0209f1abc3bb8b0c6c0e747bb7875d048d26f8ed
jehyunJung <toojey7@gmail.com>
2023-03-10 20:47:44 +0900

    feat: add method based authorization

---
+++ src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
@@ -0,0 +1,18 @@
+package io.security.corespringsecurity.aopsecurity;
+
+import io.security.corespringsecurity.domain.dto.AccountDto;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AopSecurityController {
+
+    @GetMapping("/preAuthorize")
+    @PreAuthorize("hasRole('ROLE_USER') and #accountDto.username == principal.username")
+    public String preAuthorize(AccountDto accountDto, Model model) {
+        model.addAttribute("method", "Success @PreAuthorize");
+        return "aop/method";
+    }
+}
