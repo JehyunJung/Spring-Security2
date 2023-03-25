7ecb0d82a758857e012428a0321b5b64a53af126
jehyunJung <toojey7@gmail.com>
2023-03-25 11:48:22 +0900

    view: add views for method authorization

--- src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
+++ src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
@@ -1,18 +1,30 @@
 package io.security.corespringsecurity.aopsecurity;
 
 import io.security.corespringsecurity.domain.dto.AccountDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AopSecurityController {
+
+    private final AopMethodService aopMethodService;
 
     @GetMapping("/preAuthorize")
     @PreAuthorize("hasRole('ROLE_USER') and #accountDto.username == principal.username")
     public String preAuthorize(AccountDto accountDto, Model model) {
         model.addAttribute("method", "Success @PreAuthorize");
+        return "aop/method";
+    }
+
+    @GetMapping("/methodSecured")
+    public String methodSecured(Model model) {
+        aopMethodService.methodSecured();
+        model.addAttribute("method", "Success Method Secured");
+
         return "aop/method";
     }
 }
